5f82423693c935ed4749cba6d70b2b5973456b86
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-10-07 21:49:26 -0400

    Progress upload - templates created

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page. '/' */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
-/* GET home page. */
+/* GET home page. '/home' */
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
 /* GET About Us page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About'});
+  res.render('template', { title: 'About'});
 });
 
 /* GET Products page. */
 router.get('/portfolio', function(req, res, next) {
-  res.render('index', { title: 'Portfolio'});
+  res.render('template', { title: 'Portfolio'});
 });
 
 /* GET Services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services'});
+  res.render('template', { title: 'Services'});
 });
 
 /* GET Contact Us page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact'});
+  res.render('template', { title: 'Contact'});
 });
 
 module.exports = router;
