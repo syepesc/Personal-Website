abed1efa0505b2e83480eef988b93bc5c24bbaa7
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-10-08 23:49:10 -0400

    Final draft - website done (updates could be applied)

--- public/Scripts/app.js
+++ public/Scripts/app.js
@@ -1,3 +1,6 @@
+const { event } = require("jquery");
+const { format } = require("morgan");
+
 // IIFE -- Immediately Invoked Function Expression
 (function(){
 
@@ -5,6 +8,28 @@
     {
         console.log("App Started...");
     }
+
+    if (document.title == "Contact") {
+        let sendButton = document.getElementById("submit");
+        let form = document.forms[0];
+
+        sendButton.addEventListener("click", (event) => {
+            event.preventDefault();
+
+            let fullName = document.getElementById("fullName").Value;
+            let contactNumber = document.getElementById("contactNumber").Value;
+            let email = document.getElementById("email").Value;
+            let message = document.getElementById("message").Value;
+
+            console.info(`Full name: ${fullName}
+            Contact Number: ${contactNumber}
+            Email: ${email}
+            Message: ${message}`);
+
+            form.reset();
+        });
+    }
+
 
     window.addEventListener("load", Start);
 
