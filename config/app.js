da6be2f24f004b4cb2c9c34e9e6b7a84a24e3b9a
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-10-19 22:34:37 -0400

    added conection with damongo database

--- config/app.js
+++ config/app.js
@@ -1,27 +1,44 @@
+// third parties packages
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+// DATABASE SETUP
+let mongoose = require('mongoose');
+let DB = require('./db');
+
+// point mongoose to the DB URI
+mongoose.connect(DB.URI, {useNewUrlParser: true, useUnifiedTopology: true});
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console, 'Connection Error: '));
+mongoDB.once('open', ()=>{
+  console.log('Connected to MongoDB...');
+});
+
+// set up routes
+var indexRouter = require('../routes/index');
+var usersRouter = require('../routes/users');
+let booksRouter = require('../routes/book');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs'); // express -e (to install standard folder structure)
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'node_modules'))); // added to predetermine the path for libraries used inside node modules
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../node_modules'))); // added to predetermine the path for libraries used inside node modules
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/book-list', booksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
