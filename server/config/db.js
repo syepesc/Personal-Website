4d80626d783ca9761a91e7190e2562e34b820de7
Santiago Yepes <santiagoyepescarrera@gmail.com>
2020-10-24 20:07:18 -0400

    add progress

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 module.exports = 
 {
     //"URI": "mongodb://localhost/book_store"
-    "URI": "mongodb+srv://santiago:Syc88199388@mongodbserver.p6m24.mongodb.net/book_store?retryWrites=true&w=majority"
+    "URI": "mongodb+srv://santiago:9dJdWpumKsUQIrgA@mongodbserver.p6m24.mongodb.net/book_store?retryWrites=true&w=majority"
 }
     
